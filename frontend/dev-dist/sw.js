6241429545942256b2ce32e3a336a2f6a64b87d9
parthgupta <pgparthgupta14@gmail.com>
2026-02-22 17:29:02 +0530

    updated project

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dol65e4seuk"
+    "revision": "0.aekvem4aibo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
